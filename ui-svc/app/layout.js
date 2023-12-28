a44e0e7ec4b1db31a5e3f79dd2a168d9046a17ae
Kahn <i@coooooc.com>
2023-12-28 18:36:50 +0800

    add shadcn/ui

--- ui-svc/app/layout.js
+++ ui-svc/app/layout.js
@@ -1,16 +1,20 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import {Inter} from 'next/font/google'
+import Sidebar from "@/components/sidebar";
+import '@/styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: '',
+	title: 'SR-MS',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({children}) {
+	return (
+		<html lang="en">
+		<body className={inter.className}>
+			<Sidebar />
+			{children}
+		</body>
+		</html>
+	)
 }
